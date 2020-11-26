9c967109d8ee43c20ff786768aba8ab97276337e
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-11-26 14:49:13 -0500

    try

--- src/components/item2.js
+++ src/components/item2.js
@@ -4,7 +4,6 @@ function Item2(props) {
   let height = window.innerHeight;
   let width = window.innerWidth;
   return (
-      // <span style={{position:"absolute" ,left:`${props.n*4-5}vw`, top: `-20vh`,font:"", fontSize:'4vw', color: "red", zIndex:"400"}} >{props.charItem}</span>
       <span className='letterStyle' style={{left:`${props.n*6.5-Math.floor(40* Math.sqrt(width/height))}vw` }} >{props.charItem}</span>
   );
 }
