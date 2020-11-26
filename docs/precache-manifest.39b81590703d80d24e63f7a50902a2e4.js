a888e065e519838d13bdbfbabe818ed33d3ccf0d
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-11-26 11:19:30 -0500

    deploy

---
+++ docs/precache-manifest.39b81590703d80d24e63f7a50902a2e4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874ef58631c08a01f366412ecbf35968",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d544c62c0d57aceebf1b",
+    "url": "/react-portfolio/static/css/2.f56bb660.chunk.css"
+  },
+  {
+    "revision": "8c34d7fb6ac24dadba7a",
+    "url": "/react-portfolio/static/css/main.29b58ab5.chunk.css"
+  },
+  {
+    "revision": "d544c62c0d57aceebf1b",
+    "url": "/react-portfolio/static/js/2.a4d3468e.chunk.js"
+  },
+  {
+    "revision": "cd2cf42b93a8e8d675f1265d66641d00",
+    "url": "/react-portfolio/static/js/2.a4d3468e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c34d7fb6ac24dadba7a",
+    "url": "/react-portfolio/static/js/main.6d6a46df.chunk.js"
+  },
+  {
+    "revision": "f62bc9bb79a6cc2f90046d7318671d8c",
+    "url": "/react-portfolio/static/js/pdf.worker.entry.f549feca.worker.js"
+  },
+  {
+    "revision": "ac3df35bcd7a2aa579e062210ce2d521",
+    "url": "/react-portfolio/static/js/pdf.worker.entry.f549feca.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
